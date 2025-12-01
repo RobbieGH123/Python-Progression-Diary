810123da32ab0966124bfe542a9b765cff758f25
RobbieGH123 <Robbieladd15@icloud.com>
2025-12-01 09:48:10 +0000

    Removed web design elements from my Python progression diary

--- express-rest-api/models/users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');                      // Import Mongoose library so that I can interact with MongoDB and create schemas
-
-const userSchema = new mongoose.Schema({                   // A Schema describes the structure for documents within a collection
-    name: { type: String, required: true, unique: true }   // Name is the sole field, Must be a string, required and not a duplicate
-});
-
-module.exports = mongoose.model('User', userSchema);
-
-// Exportable | Register with mongo DB | Named User and based off the Schema
--- src/Javascript-warmup/Index.js
+++
@@ -1,4 +0,0 @@
-const math = require('./math'); // Sets the object to the constant variable math
-
-console.log(math.add(6, 7));
-console.log(math.subtract(6, 7));
